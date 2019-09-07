c644f6eef3cddddddb48e7c60df56bd64090e37f
CodeBlastr <github@razorit.com>
2019-09-07 09:45:06 -0400

    test selenium drivers

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -7,4 +7,18 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals($greeting, $requiredGreeting);
     }
+
+    public function testSeleniumSauce() {
+        $caps = Selenium::WebDriver::Remote::Capabilities.firefox([
+          'tunnel-identifier' => env('TRAVIS_JOB_NUMBER')
+        ]);
+//        $driver = Selenium::WebDriver.for(:remote, json_encode('{
+//                    url: "http://username:access_key@ondemand.saucelabs.com/wd/hub",
+//          desired_capabilities: "' . $caps . '"
+//        }'));
+
+        print_r($caps, true);
+
+        $this->assertEquals(false, true);
+    }
 }
